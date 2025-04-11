125faf20a4c113ff6a6a047594aaa8ecda54faf1
Miguel Soeth <61883339+miguelsoeth@users.noreply.github.com>
2025-04-11 19:20:12 -0300

    Inicio de desenvolvimento dos locais

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Autenticacao from "./pages/Autenticacao";
 import Eventos from "./pages/Eventos";
 import RotaProtegida from "./auth/RotaProtegida";
+import Locais from "./pages/Locais";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Autenticacao />} />
         <Route path="/eventos" element={<RotaProtegida><Eventos /></RotaProtegida>} />
+        <Route path="/locais" element={<RotaProtegida><Locais /></RotaProtegida>} />
       </Routes>
     </Router>
   );
