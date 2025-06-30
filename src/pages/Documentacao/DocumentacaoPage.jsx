fd724d25959fe2cf09d56b60798f999fdfd669a6
Miguel Soeth <61883339+miguelsoeth@users.noreply.github.com>
2025-06-29 23:07:51 -0300

    redirect manual HTML

--- src/pages/Documentacao/DocumentacaoPage.jsx
+++ src/pages/Documentacao/DocumentacaoPage.jsx
@@ -1,26 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import ReactMarkdown from 'react-markdown';
-import rehypeRaw from 'rehype-raw';
+import { useEffect } from 'react';
 import 'github-markdown-css'; 
 
 const DocumentacaoPage = () => {
-  const [markdown, setMarkdown] = useState('');
-
   useEffect(() => {
-    fetch('/documentacao.md')
-      .then((res) => res.text())
-      .then((text) => setMarkdown(text))
-      .catch((err) => console.error('Erro ao carregar Markdown:', err));
+    window.location.href = `${window.location.origin}/documentacao.html`;
   }, []);
 
-  return (
-    <div className="markdown-body" style={{ padding: '2rem' }}>
-      <ReactMarkdown rehypePlugins={[rehypeRaw]}>
-        {markdown}
-      </ReactMarkdown>
-    </div>
-  );
+  return null;
 };
-
 
 export default DocumentacaoPage;
